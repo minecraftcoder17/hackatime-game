ea1e8210fe066a07f5e9da835230f3b4db615e88
minecraftcoder17 <minecraftcoder17@gmail.com>
2026-08-20 15:03:26 -0400

    added a player, added a tilemap (will add collision soon)

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.vy = -50
+})
+let mySprite: Sprite = null
+mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+mySprite.ay = -4
+tiles.setCurrentTilemap(tilemap`level`)
+forever(function () {
+    scene.cameraFollowSprite(mySprite)
+    mySprite.vy += 1
+})
+game.onUpdateInterval(500, function () {
+	
+})
