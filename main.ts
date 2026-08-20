b0a0b10e4ef535a0cbdb0ac4ba2d687a171f478c
minecraftcoder17 <minecraftcoder17@gmail.com>
2026-08-20 16:23:05 -0400

    Added spikes, edited movement speed

--- main.ts
+++ main.ts
@@ -14,30 +14,37 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
         MainPlayer.setImage(assets.image`left`)
     }
 })
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    MainPlayer.setPosition(10, 61)
+})
 let MainPlayer: Sprite = null
 let MoveCooldown = 0
+tileUtil.connectMaps(tilemap`level`, tilemap`level2`, MapConnectionKind.Door1)
 MoveCooldown = -1
 scene.setBackgroundColor(9)
 MainPlayer = sprites.create(assets.image`front`, SpriteKind.Player)
 MainPlayer.ay = 300
 tiles.setCurrentTilemap(tilemap`level`)
+MainPlayer.fx = 300
 forever(function () {
     scene.cameraFollowSprite(MainPlayer)
     if (100 + MoveCooldown < game.runtime()) {
         if (controller.left.isPressed()) {
-            MainPlayer.vx = -50
+            MainPlayer.vx = -90
             MainPlayer.setImage(assets.image`left`)
         } else {
             if (controller.right.isPressed()) {
-                MainPlayer.vx = 50
+                MainPlayer.vx = 90
                 MainPlayer.setImage(assets.image`right`)
             } else {
-                MainPlayer.vx = 0
+            	
             }
         }
     }
     if (MainPlayer.x > 240) {
         tiles.setCurrentTilemap(tilemap`level2`)
+        MainPlayer.setPosition(10, 61)
+        MainPlayer.setVelocity(0, 0)
     } else {
     	
     }
