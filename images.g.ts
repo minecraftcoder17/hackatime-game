64d42c018ca819b019ae78791be09f785812a822
minecraftcoder17 <minecraftcoder17@gmail.com>
2026-08-20 16:01:37 -0400

    Updated some sprites, added flowers as decoration, added wall jumping

--- images.g.ts
+++ images.g.ts
@@ -3,24 +3,62 @@ namespace myImages {
 
     helpers._registerFactory("image", function(name: string) {
         switch(helpers.stringTrim(name)) {
+            case "image2":
+            case "left":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f f f . . . 
+. . f 1 1 1 1 1 1 1 1 1 d f . . 
+. . f 1 1 1 1 1 1 1 1 1 d f . . 
+. . f 1 f 1 1 f 1 1 1 1 d f . . 
+. . f 1 f 1 1 f 1 1 1 1 d f . . 
+. . f 1 1 1 1 1 1 1 1 1 d f . . 
+. . f 1 1 1 1 1 1 1 1 1 d f . . 
+. . f 1 1 f f f 1 1 1 1 d f . . 
+. . f 1 1 1 1 1 1 1 1 1 d f . . 
+. . f 1 1 1 1 1 1 1 1 1 d f . . 
+. . f d d d d d d d d d d f . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "right":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f f f . . . 
+. . f d 1 1 1 1 1 1 1 1 1 f . . 
+. . f d 1 1 1 1 1 1 1 1 1 f . . 
+. . f d 1 1 1 1 f 1 1 f 1 f . . 
+. . f d 1 1 1 1 f 1 1 f 1 f . . 
+. . f d 1 1 1 1 1 1 1 1 1 f . . 
+. . f d 1 1 1 1 1 1 1 1 1 f . . 
+. . f d 1 1 1 1 f f f 1 1 f . . 
+. . f d 1 1 1 1 1 1 1 1 1 f . . 
+. . f d 1 1 1 1 1 1 1 1 1 f . . 
+. . f d d d d d d d d d d f . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
             case "image1":
-            case "myImage":return img`
-. f f f f f f f f f f f f f f . 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 f 1 1 1 1 f 1 1 1 1 f 
-f 1 1 1 1 f 1 1 1 1 f 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 f f f f 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-f 1 1 1 1 1 1 1 1 1 1 1 1 1 1 f 
-. f f f f f f f f f f f f f f . 
+            case "front":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . f f f f f f f f f f . . . 
+. . f 1 1 1 1 1 1 1 1 1 1 f . . 
+. . f 1 1 1 1 1 1 1 1 1 1 f . . 
+. . f 1 1 1 f 1 1 f 1 1 1 f . . 
+. . f 1 1 1 f 1 1 f 1 1 1 f . . 
+. . f 1 1 1 1 1 1 1 1 1 1 f . . 
+. . f 1 1 1 1 1 1 1 1 1 1 f . . 
+. . f 1 1 1 f f f f 1 1 1 f . . 
+. . f 1 1 1 1 1 1 1 1 1 1 f . . 
+. . f 1 1 1 1 1 1 1 1 1 1 f . . 
+. . f d d d d d d d d d d f . . 
+. . . f f f f f f f f f f . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
 `;
         }
         return null;
--- main.ts
+++ main.ts
@@ -3,24 +3,42 @@ namespace SpriteKind {
 }
 controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (MainPlayer.isHittingTile(CollisionDirection.Bottom)) {
-        MainPlayer.vy = -100
+        MainPlayer.vy = -150
+    } else if (MainPlayer.isHittingTile(CollisionDirection.Left)) {
+        MainPlayer.setVelocity(50, -50)
+        MoveCooldown = game.runtime()
+        MainPlayer.setImage(assets.image`right`)
+    } else if (MainPlayer.isHittingTile(CollisionDirection.Right)) {
+        MainPlayer.setVelocity(-50, -50)
+        MoveCooldown = game.runtime()
+        MainPlayer.setImage(assets.image`left`)
     }
 })
 let MainPlayer: Sprite = null
+let MoveCooldown = 0
+MoveCooldown = -1
 scene.setBackgroundColor(9)
-MainPlayer = sprites.create(assets.image`myImage`, SpriteKind.Player)
-MainPlayer.ay = 200
+MainPlayer = sprites.create(assets.image`front`, SpriteKind.Player)
+MainPlayer.ay = 300
 tiles.setCurrentTilemap(tilemap`level`)
-MainPlayer.setBounceOnWall(false)
 forever(function () {
     scene.cameraFollowSprite(MainPlayer)
-    if (controller.left.isPressed()) {
-        MainPlayer.vx = -50
-    } else {
-        if (controller.right.isPressed()) {
-            MainPlayer.vx = 50
+    if (100 + MoveCooldown < game.runtime()) {
+        if (controller.left.isPressed()) {
+            MainPlayer.vx = -50
+            MainPlayer.setImage(assets.image`left`)
         } else {
-            MainPlayer.vx = 0
+            if (controller.right.isPressed()) {
+                MainPlayer.vx = 50
+                MainPlayer.setImage(assets.image`right`)
+            } else {
+                MainPlayer.vx = 0
+            }
         }
+    }
+    if (MainPlayer.x > 240) {
+        tiles.setCurrentTilemap(tilemap`level2`)
+    } else {
+    	
     }
 })
