cd3f2555ac587a4c429d5e6ee20f31ebfd713147
minecraftcoder17 <minecraftcoder17@gmail.com>
2026-08-20 15:20:43 -0400

    Added collision, jumping, and slightly updated graphics

--- main.ts
+++ main.ts
@@ -1,14 +1,26 @@
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    mySprite.vy = -50
+namespace SpriteKind {
+    export const Ground = SpriteKind.create()
+}
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (MainPlayer.isHittingTile(CollisionDirection.Bottom)) {
+        MainPlayer.vy = -100
+    }
 })
-let mySprite: Sprite = null
-mySprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
-mySprite.ay = -4
+let MainPlayer: Sprite = null
+scene.setBackgroundColor(9)
+MainPlayer = sprites.create(assets.image`myImage`, SpriteKind.Player)
+MainPlayer.ay = 200
 tiles.setCurrentTilemap(tilemap`level`)
+MainPlayer.setBounceOnWall(false)
 forever(function () {
-    scene.cameraFollowSprite(mySprite)
-    mySprite.vy += 1
-})
-game.onUpdateInterval(500, function () {
-	
+    scene.cameraFollowSprite(MainPlayer)
+    if (controller.left.isPressed()) {
+        MainPlayer.vx = -50
+    } else {
+        if (controller.right.isPressed()) {
+            MainPlayer.vx = 50
+        } else {
+            MainPlayer.vx = 0
+        }
+    }
 })
