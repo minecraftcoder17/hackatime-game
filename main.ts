ec37ecffb961ba4990f3c7ccc1330f6a56176a80
minecraftcoder17 <minecraftcoder17@gmail.com>
2026-08-20 17:23:17 -0400

    Added checkpoints

--- main.ts
+++ main.ts
@@ -6,28 +6,37 @@ controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
     if (MainPlayer.isHittingTile(CollisionDirection.Bottom) || MainPlayer.isHittingTile(CollisionDirection.Top)) {
         MainPlayer.vy = MainPlayer.ay / -4
     } else if (MainPlayer.isHittingTile(CollisionDirection.Left)) {
-        MainPlayer.setVelocity(50, -50)
+        MainPlayer.setVelocity(50, MainPlayer.ay / -4)
         MoveCooldown = game.runtime()
         MainPlayer.setImage(assets.image`right`)
     } else if (MainPlayer.isHittingTile(CollisionDirection.Right)) {
-        MainPlayer.setVelocity(-50, -50)
+        MainPlayer.setVelocity(-50, MainPlayer.ay / -4)
         MoveCooldown = game.runtime()
         MainPlayer.setImage(assets.image`left`)
     }
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile6`, function (sprite, location) {
-    MainPlayer.setPosition(10, 61)
+    MainPlayer.setPosition(spawnx, spawny)
+    MainPlayer.ay = 600
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile7`, function (sprite, location) {
+    spawnx = MainPlayer.x
+    spawny = MainPlayer.y
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
-    MainPlayer.setPosition(10, 61)
+    MainPlayer.setPosition(spawnx, spawny)
+    MainPlayer.ay = 600
 })
 controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
     if (CanFlipGravity == 1) {
         MainPlayer.ay = MainPlayer.ay * -1
         CanFlipGravity = 0
+        MainPlayer.startEffect(effects.blizzard, 500)
     }
 })
 let CanFlipGravity = 0
+let spawny = 0
+let spawnx = 0
 let MainPlayer: Sprite = null
 let MoveCooldown = 0
 tileUtil.connectMaps(tilemap`level`, tilemap`level2`, MapConnectionKind.Door1)
@@ -39,6 +48,8 @@ MainPlayer.ay = 600
 tiles.setCurrentTilemap(tilemap`level`)
 MainPlayer.fx = 500
 MainPlayer.sayText("Press \"Down\"", 5000, false)
+spawnx = 10
+spawny = 61
 forever(function () {
     scene.cameraFollowSprite(MainPlayer)
     if (100 + MoveCooldown < game.runtime()) {
